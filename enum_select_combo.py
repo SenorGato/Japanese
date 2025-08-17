4ffe1f31be512ebabd275fd8802d539c8e356d8e
Claus Mouse <11484197-senoraraton@users.noreply.gitlab.com>
2025-08-16 23:49:35 -0700

    Squashed 'japanese/ajt_common/' content from commit 68afdb8
    git-subtree-dir: japanese/ajt_common
    git-subtree-split: 68afdb80c4c98578c2198480acea5ff03d0043d2

---
+++ enum_select_combo.py
@@ -0,0 +1,37 @@
+# Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+import enum
+
+from aqt.qt import *
+
+from .utils import ui_translate
+
+
+class EnumSelectCombo(QComboBox):
+    def __init__(
+        self,
+        enum_type: enum.EnumMeta,
+        initial_value: Union[enum.Enum, str, None] = None,
+        show_values: bool = False,
+        parent=None,
+    ) -> None:
+        super().__init__(parent)
+        for item in enum_type:
+            self.addItem(ui_translate(item.value if show_values else item.name), item)
+        if initial_value is not None:
+            self.setCurrentName(initial_value)
+
+    def setCurrentName(self, name: Union[enum.Enum, str]) -> None:
+        for index in range(self.count()):
+            if self.itemData(index) == name or self.itemData(index).name == name:
+                return self.setCurrentIndex(index)
+
+    def currentName(self) -> str:
+        return self.currentData().name
+
+    def setCurrentText(self, text: str) -> None:
+        raise NotImplementedError()
+
+    def currentText(self) -> str:
+        raise NotImplementedError()
